8785ffa62410e6833948eec85418d0cf82f6438a
Ozzy <ocardenas789@me.com>
2024-02-22 15:56:29 -0600

    update ignore files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ const config = {
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:storybook/recommended",
     "plugin:storybook/recommended"
   ],
   rules: {
