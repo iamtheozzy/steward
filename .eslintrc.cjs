f2336062628f359449e8193b03748b9f8adddf1c
Ozzy <ocardenas789@me.com>
2024-02-28 22:10:34 -0600

    remove storybook from linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,8 +9,6 @@ const config = {
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
-    "plugin:storybook/recommended",
-    "plugin:storybook/recommended"
   ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
